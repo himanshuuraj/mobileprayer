7f40d62e49b1f3f622bc8a0afd37c437a678f941
Himanshu <himanshu.raj@spotdy.com>
2017-05-04 20:06:56 +0530

    injected http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+//import { Http } from '@angular/http'
+//import {HTTP_PROVIDERS} from 'angular/http';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -16,6 +19,7 @@ import { Prayercircle } from '../pages/prayercircle/prayercircle';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 //import { Contacts } from '@ionic-native';
 import {Prayercircle} from '../prayercircle/prayercircle';
+import { Http, Headers, RequestOptions } from '@angular/http';
+//import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -9,12 +12,32 @@ import {Prayercircle} from '../prayercircle/prayercircle';
 })
 export class HomePage {
   aboutPage = Prayercircle;
-  constructor(public navCtrl: NavController) {
-    //console.log(Contacts);
-    //this.aboutPage = prayercircle;
+  constructor(public navCtrl: NavController, private http:Http) {
+
   }
 
   openFilters(){
     this.navCtrl.setRoot(this.aboutPage);
   }
+
+  sendData(){
+      var headers = new Headers();
+      headers.append("Accept", 'application/json');
+     headers.append('Content-Type', 'application/json' );
+     let options = new RequestOptions({ headers: headers });
+
+     let postParams = {
+       title: 'foo',
+       body: 'bar',
+       userId: 1
+     }
+
+     this.http.post("http://jsonplaceholder.typicode.com/posts", postParams, options)
+       .subscribe(data => {
+         console.log(data['_body']);
+        }, error => {
+         console.log(error);// Error getting the data
+       });
+  }
+
 }
